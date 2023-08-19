a58faf9486b035634aaacb5c7dbb2066a0d59197
walz0 <aidan.m.walz@gmail.com>
2023-08-19 04:32:57 -0400

    progress on loading rst anims, very messy

--- Scenes/Ranked/Results/RstScreen.h
+++ Scenes/Ranked/Results/RstScreen.h
@@ -8,6 +8,116 @@
 #include "../../Components/TurnIndicator.h"
 #include "../../ExiSlippi.h"
 
+typedef struct PlayerCreateArgs {
+  int character_id;
+  u8 slot;
+  u8 unk;
+  u8 flags2;
+  u8 unk1;
+} PlayerCreateArgs;
+
+typedef struct AnimationInfo {
+  u8 lerp_frames;
+  u8 phys_joint_bone_index;
+} AnimationInfo;
+
+typedef struct SubactionInfo {
+    char *symbol;
+    int animOffset;
+    int animSize;
+    char *script;
+    int anim_flags;
+    HSD_Archive *anim_data;
+} SubactionInfo;
+
+typedef struct ActionStateInfo {
+  int subaction;
+  int action_stat;
+  u8 unk;
+  u8 unk1;
+  u8 unk2;
+  u8 unk3;
+  void *animation_handler;
+  void *interrupt_handler;
+  void *physics_handler;
+  void *collision_handler;
+  void *camera_handler;
+} ActionStateInfo;
+
+typedef struct StartMeleeRules
+{
+    u8 flags1;             // 0x0
+    u8 flags2;                
+    u8 flags3;                // 0x4, temp transfer buffer used during CARDReads. is 0x2000 bytes
+    u8 flags4;                // 0x4, temp transfer buffer used during CARDReads. is 0x2000 bytes
+    u8 flags5;                // 0x4, temp transfer buffer used during CARDReads. is 0x2000 bytes
+    u8 flags6;                      // 0x8
+    u8 xc;                      // 0xc
+    u8 x10;                     // 0x10
+    int is_teams;                     // 0x14
+    u8 x20;                     // 0x20
+    u8 x24;                     // 0x24
+    u8 item_frequency;                     // 0x28
+    u8 sd_penalty;                     // 0x28
+    u8 unk3;                     // 0x28
+    u16 stage_id;                     // 0x28
+    int time_limit;
+    u8 unk4;                     // 0x28
+    u8 unk5;                     // 0x28
+    u8 unk6;                     // 0x28
+    u8 unk7;                     // 0x28
+    u8 unk8;                     // 0x28
+    u8 unk9;                     // 0x28
+    u8 unk10;                     // 0x28
+    u8 unk11;                     // 0x28
+    u8 unk12;                     // 0x28
+    u8 unk13;                     // 0x28
+    u8 unk14;                     // 0x28
+    u8 unk15;                     // 0x28
+    long item_mask;                     // 0x28
+    u8 unk16;                     // 0x28
+    u8 unk17;                     // 0x28
+    u8 unk18;                     // 0x28
+    u8 unk19;                     // 0x28
+    u8 unk20;                     // 0x28
+    u8 unk21;                     // 0x28
+    u8 unk22;                     // 0x28
+    u8 unk23;                     // 0x28
+    float damage_ratio;                     // 0x28
+    float game_speed;                     // 0x28
+    u8 unk24;                     // 0x28
+    u8 unk25;                     // 0x28
+    u8 unk26;                     // 0x28
+    u8 unk27;                     // 0x28
+    u8 unk28;                     // 0x28
+    u8 unk29;                     // 0x28
+    u8 unk30;                     // 0x28
+    u8 unk31;                     // 0x28
+    void* pause_check_callback;                     // 0x28
+    u8 unk32;                     // 0x28
+    u8 unk33;                     // 0x28
+    u8 unk34;                     // 0x28
+    u8 unk35;                     // 0x28
+    u8 unk36;                     // 0x28
+    u8 unk37;                     // 0x28
+    u8 unk38;                     // 0x28
+    u8 unk39;                     // 0x28
+    u8 unk40;                     // 0x28
+    u8 unk41;                     // 0x28
+    u8 unk42;                     // 0x28
+    u8 unk43;                     // 0x28
+    void* match_end_callback;                     // 0x28
+    int unk44;                     // 0x28
+    u8 unk45;                     // 0x28
+    u8 unk46;                     // 0x28
+    u8 unk47;                     // 0x28
+    u8 unk48;                     // 0x28
+    u8 unk49;                     // 0x28
+    u8 unk50;                     // 0x28
+    u8 unk51;                     // 0x28
+    u8 unk52;                     // 0x28
+} StartMeleeRules;
+
 // This struct is defined in asm via directives and has no padding, so we need to use pack(1)
 #pragma pack(1)
 typedef struct RstScreen_SceneData {
@@ -20,20 +130,58 @@ typedef struct RstScreen_SceneData {
 } RstScreen_SceneData;
 #pragma pack()
 
+typedef struct StartMeleeData {
+  StartMeleeRules rules; 
+  PlayerData *players;
+} StartMeleeData;
+
 int (*GetCharacterVictoryTheme)(CharacterKind kind) = (int *) 0x80160400;
 int (*SFX_PlayVolPitch)(int sfx, int volume, int pitch) = (int *) 0x800237a8;
 JOBJ* (*VictoryScreen_GetWinnerLogoJObj)(JOBJ *jobj, CharacterKind ckind, int is_quit_out) = (JOBJ *)0x80176bf0;
 void* (*VictoryScreen_CreateWinnerLogo)(void) = (void *) 0x80176f60;
+GOBJ* (*Match_SetupPlayerVictoryPose)(CharacterKind kind, int costume_id, uint slot) = (GOBJ *)0x8017a67c;
+void* (*LoadEffects)(int unk) = 0x8006737c;
+GOBJ* (*VictoryPose_Decide)(int *kind) = (GOBJ *) 0x800be7e0;
+void* (*Player_Free)(FighterData *player) = (void *) 0x8006dabc;
+void* (*Player_InitializeFromInfo)(GOBJ *playerGobj, PlayerCreateArgs *args) = (void *) 0x80068914;
+void* (*Player_LoadEffects)(int param_1) = (void *) 0x8006737c;
+void* (*SetupPlayerSlot)(int slot) = (void *) 0x80031ad0;
+
+void* (*Match_Init)(StartMeleeData *data) = (void *) 0x8016dcc0;
+void* (*InitializeStartMeleeData)(StartMeleeData *data) = (void *) 0x80167a64;
+
+// ActionStateInfo* (COMMON_ACTION_STATES) = 0x803c2800;
+// ActionStateInfo* (CHAR_ACTION_STATES) = 0x803c12e0;
+
+FtState* (COMMON_ACTION_STATES) = 0x803c2800;
+FtState** (CHAR_ACTION_STATES) = 0x803c12e0;
 
 // SFX
 int NARRATOR_WINNER_IS = 0xC355;
 int BACKGROUND_NOISE = 0x48; // Cheering
 
+// GLOBALS
 int currentSFX = 0x7530;
-
 int debugText;
 char* debugString[20];
 Text *text;
+int framesLeft;
+
+int charSFX;
+
+int FRAMES_WINNER_IS = 100;
+
+// #define VICTORY_SCREEN_DATA(offset) *(u8 *)(0x8046dbe8 + offset)
+// u8 individual_winner = VICTORY_SCREEN_DATA(4);
+// u8 team_winner = VICTORY_SCREEN_DATA(5);
+// u8 winner = VICTORY_SCREEN_DATA(6);
+// int pnlsce = VICTORY_SCREEN_DATA(16);
+// int flmsce = VICTORY_SCREEN_DATA(20);
+// int think_gobj = VICTORY_SCREEN_DATA(24);
+// int cobj = VICTORY_SCREEN_DATA(28);
+// JOBJ *winner_logo = VICTORY_SCREEN_DATA(32);
+// int last_match = VICTORY_SCREEN_DATA(148);
+// int players = VICTORY_SCREEN_DATA(152);
 
 typedef struct CharThumbnail {
   FighterKind kind;
@@ -149,8 +297,13 @@ static CharNameSFX NARRATOR_NAMES[33] = {
   {CKIND_SANDBAG, 0},
 };
 
-typedef struct MatchResult {
-  int test;
+typedef enum MatchResult {
+  NONE = 0x0,
+  TIMEOUT = 0x1,
+  ELIMINATION = 0x2,
+  LRASTART = 0x7,
+  RETRY = 0x8,
+  UNK = 0x9
 } MatchResult;
 
 typedef struct MatchTeamData {
@@ -186,13 +339,7 @@ typedef struct MatchController {
   u8 unk6[2571];
 } MatchController;
 
-typedef struct PlayerCreateArgs {
-    int character_id;
-    u8 slot;
-    u8 unk;
-    u8 flags2;
-    u8 unk1;
-} PlayerCreateArgs;
+
 
 typedef struct CostumeModelInfo {
   JOBJDesc *desc;
